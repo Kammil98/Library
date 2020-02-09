38842c13d749649a8ce9fa167c3c549ca5b48fb2
Agere <bmateusz1998@gmail.com>
2020-02-09 01:25:11 +0100

    Add Required attributes to book model

--- Library_WebApp/LibraryApp/Models/Book.cs
+++ Library_WebApp/LibraryApp/Models/Book.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace LibraryApp.Models
-{
-    public partial class Book
-    {
-        public Book()
-        {
+namespace LibraryApp.Models {
+    public partial class Book {
+        public Book() {
             Authorship = new HashSet<Authorship>();
             Edition = new HashSet<Edition>();
         }
 
         public int Id { get; set; }
+        [Required(ErrorMessage = "To pole jest wymagane")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "To pole jest wymagane")]
         public string Genre { get; set; }
 
         public virtual Genre GenreNavigation { get; set; }
