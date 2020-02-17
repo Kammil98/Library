1c95a966f82004adfa19bf5f0d08e7ff599922d6
Agere <bmateusz1998@gmail.com>
2020-02-17 15:14:50 +0100

    Create reservations controller

---
+++ Library_WebApp/LibraryApp/Controllers/ReservationsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LibraryApp;
+using LibraryApp.Models;
+
+namespace LibraryApp.Controllers {
+
+    public class ReservationsController : Controller {
+
+        private readonly LibraryContext _context;
+
+        public ReservationsController(LibraryContext context) {
+            _context = context;
+        }
+
+        // GET: Reservations
+        public IActionResult Index(int? id) {
+            return RedirectToAction(nameof(BookCopiesController.Details), "BookCopies", new { id });
+        }
+
+        // POST: Reservations/Return
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Return(int copyId) {
+            await _context.ReturnCopy(copyId);
+            return RedirectToAction(nameof(Index), new { id = copyId });
+        }
+
+        // POST: Reservations/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(int copyId, string login) {
+            await _context.ReserveCopy(copyId, login);
+            return RedirectToAction(nameof(Index), new { id = copyId });
+        }
+    }
+}
--- Library_WebApp/LibraryApp/Models/Reservation.cs
+++ Library_WebApp/LibraryApp/Models/Reservation.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace LibraryApp.Models
-{
-    public partial class Reservation
-    {
+namespace LibraryApp.Models {
+    public partial class Reservation {
         public string UserLogin { get; set; }
         public int CopyId { get; set; }
+        [DataType(DataType.DateTime)]
         public DateTimeOffset ReservationDate { get; set; }
 
         public virtual BookCopy Copy { get; set; }
